438afa6eb9cf636a0515118f5a9abe3b12cace32
piashlofkw-cyber <piashlofkw@gmail.com>
2025-11-27 21:48:02 +0300

    Update bus.c

--- drivers/net/wireless/cnss2/bus.c
+++ drivers/net/wireless/cnss2/bus.c
@@ -27,8 +27,11 @@ enum cnss_dev_bus_type cnss_get_bus_type(struct cnss_plat_data *plat_priv)
 
 	if (plat_priv->is_converged_dt) {
 		dev = &plat_priv->plat_dev->dev;
+		u32 val = 0;
 		ret = of_property_read_u32(dev->of_node, "qcom,bus-type",
-					   &bus_type);
+					   &val);
+		if (!ret)
+			bus_type = val;
 		if (!ret && bus_type <= CNSS_BUS_USB)
 			cnss_pr_dbg("Got bus type[%u] from dt\n", bus_type);
 		else
