5eb8e7e7f5381455808d3c0d1ff274f1e53036cc
piashlofkw-cyber <piashlofkw@gmail.com>
2025-11-27 21:49:42 +0300

    Update synaptics_dsx_core.c

--- drivers/nubia/touch/synaptics_dsx/synaptics_dsx_core.c
+++ drivers/nubia/touch/synaptics_dsx/synaptics_dsx_core.c
@@ -2056,7 +2056,7 @@ static int report_palm_event(struct synaptics_rmi4_data *rmi4_data)
 #endif
 
 #ifdef NUBIA_TOUCH_SYNAPTICS
-static void synaptics_nubiaForcedResume()
+static void synaptics_nubiaForcedResume(void)
 {
 	while((g_rmi4_data->fp_event_count < FORCED_RESUME)&&
 			g_rmi4_data->fp_down_input_flag) {
